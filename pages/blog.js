7251ca5c77a959b5378865bf642c2288485355cf
Shoaibv2 <skrj1337@gmail.com>
2022-07-01 14:55:45 +0500

    Add Blog Home

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,35 @@
+import styles from "../styles/Blog.module.css";
+
 export default function Blog() {
   return (
     <>
-      <div className="align-items-center text-center">
-        <h1>Blog</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
-          quisquam.
-        </p>
+      <div className="container">
+        <main className={styles.main}>
+          <div className={styles.blogpost}>
+            <h3>Xiaomi 12S Ultra will have 1-inch Sony IMX989 camera sensor</h3>
+            <p>
+              Xiaomi will introduce the 12S series on July 4, and today it
+              confirmed the trio of phones that will appear. We expect a small
+              vanilla
+            </p>
+          </div>
+          <div className={styles.blogpost}>
+            <h3>Xiaomi 12S Ultra will have 1-inch Sony IMX989 camera sensor</h3>
+            <p>
+              Xiaomi will introduce the 12S series on July 4, and today it
+              confirmed the trio of phones that will appear. We expect a small
+              vanilla
+            </p>
+          </div>
+          <div className={styles.blogpost}>
+            <h3>Xiaomi 12S Ultra will have 1-inch Sony IMX989 camera sensor</h3>
+            <p>
+              Xiaomi will introduce the 12S series on July 4, and today it
+              confirmed the trio of phones that will appear. We expect a small
+              vanilla
+            </p>
+          </div>
+        </main>
       </div>
     </>
   );
