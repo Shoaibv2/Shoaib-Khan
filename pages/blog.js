646ffb08e9438a1e66a87e6fd8f2f75418b06bce
Shoaibv2 <skrj1337@gmail.com>
2022-07-03 19:51:44 +0500

    APi End Points

--- pages/blog.js
+++ pages/blog.js
@@ -1,36 +1,32 @@
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function Blog() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("/api/blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        setBlogs(parsed);
+      });
+  }, []);
   return (
-    <>
-      <div className="container">
-        <main className={styles.main}>
-          <div className={styles.blogpost}>
-            <Link href={"/blogpost/xiaomi-12s-sony-img989"}>
-              <h3>OnePlus 7 & 7T Getting Android 12</h3>
-            </Link>
-            <p>
-              OnePlus 7 and OnePlus 7T are the company phones in line for an
-              Android 12 upgrade,
-            </p>
-          </div>
-          <div className={styles.blogpost}>
-            <h3>Poco F4 Review</h3>
-            <p>
-              The Poco F4 GT was announced and released ahead of the Poco F4
-              premiere,
-            </p>
-          </div>
-          <div className={styles.blogpost}>
-            <h3> Samsung Galaxy M53 Review</h3>
-            <p>
-              Another downgrade, and a very tangible one at that, is in the box
-              and its
-            </p>
-          </div>
-        </main>
-      </div>
-    </>
+    <div className="container">
+      <main className={styles.main}>
+        {blogs.map((blogitem) => {
+          return (
+            <div className={styles.blogpost} key={blogitem.slug}>
+              <Link href={`/blogpost/${blogitem.slug}`}>
+                <h2>{blogitem.title}</h2>
+              </Link>
+              <p>{blogitem.content.substring(0, 140)}</p>
+            </div>
+          );
+        })}
+      </main>
+    </div>
   );
 }
